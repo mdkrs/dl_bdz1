3957c70367f849f541502b2ff1a8cdb1ecff7617
mdkrs <>
2025-03-13 22:31:22 +0300

    fix

--- main.py
+++ main.py
@@ -404,7 +404,7 @@ def train_loop(num_epochs, model, train_loader, test_loader, train_loss_comp, te
     for epoch in range(num_epochs):
         model.train()
         loss, bleu = run_epoch(train_loader, model, train_loss_comp)
-        train_loss.append(loss.cpu().numpy().tolist()[0])
+        train_loss.append(loss.cpu().numpy().tolist())
         train_bleu.append(bleu)
         model.eval()
         loss, bleu = run_epoch(test_loader, model, test_loss_comp)
